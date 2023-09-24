556338ccad2fb3eeb1c90fdfc39f19af6b37fc3d
Shehzad Khan <102049709+mskDev0092@users.noreply.github.com>
2023-09-24 16:17:36 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,57 @@
 require('dotenv').config();
-const express = require('express');
+var express = require('express');
 const cors = require('cors');
 const app = express();
-
+const urlparser = require('url')
+const {MongoClient} = require('mongodb')
+const dns = require('dns');
 // Basic Configuration
 const port = process.env.PORT || 3000;
-
+const cdb = new MongoClient(process.env.URI);
+const db = cdb.db("url_service");
+const storeUrls = db.collection('urls');
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+ 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
-});
+}); 
+
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
-});
+app.post('/api/shorturl', function(req, res){
+  
+  const urlString = req.body.url;
+  
+  
+  const lookDns = dns.lookup(urlparser.parse(urlString).hostname , async(req , validAddress) => {
+   if(!validAddress){
+    res.json({error: "Invalid URL"})
+   } else {
+    const countUrls = await storeUrls.countDocuments({});
+    const urlStore = {
+      urlString,
+      short_url: countUrls
+    }
+
+    const respond = await storeUrls.insertOne(urlStore);
+    console.log(respond);
+    res.json({
+      original_url: urlString,
+      short_url: countUrls
+    });
+
+
+
+   }
+  })
+  
+  });
+
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
